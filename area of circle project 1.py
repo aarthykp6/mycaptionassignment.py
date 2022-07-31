dd09bc888f7e718e2a27244533ccbc692eabc043
AARTHY K P <105295898+aarthykp6@users.noreply.github.com>
2022-07-31 19:48:11 +0530

    Area of the circle

---
+++ area of circle project 1.py
@@ -0,0 +1,9 @@
+# Area of the cicle
+
+π = 3.14  
+Radius = float (input ("Enter the radius of the given circle: "))  
+area_of_the_circle = π * Radius * Radius  
+print (" The area of the given circle is: ", area_of_the_circle)  
+
+#Enter the radius of the given circle: 1.1
+#The area of the given circle is:  3.7994000000000008
